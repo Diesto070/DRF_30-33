14daa34507260b88371e6951dac92dfb779d57d1
Ivan <diestoapp@gmail.com>
2026-02-12 13:23:08 +0300

    hw_35.2 fix test 2

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 # Настройка статических файлов
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Куда собирать статику
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, 'static'),  # Где искать статику
+#     os.path.join(BASE_DIR, 'static'),
 # ]
 
 # Настройка медиафайлов
